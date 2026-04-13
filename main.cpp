ebe5c9f3eaa57aad87412f131da1b8e4877c2cb3
jinoh <qoiwer@naver.com>
2026-04-14 01:21:22 +0900

    Complete main.cpp

--- main.cpp
+++ main.cpp
@@ -1,22 +1,29 @@
-#include<iostream>
-#include<vector>
-
+#include <iostream>
+#include <vector>
 using namespace std;
 
-int N,M;
-vector<int> vec;
+int N, M;
+vector<int> arr;
 
-void func(int cnt) {
+void dfs(int start) {
+    if ((int)arr.size() == M) {
+        for (int i = 0; i < M; i++) {
+            cout << arr[i];
+            if (i != M - 1) cout << ' ';
+        }
+        cout << '\n';
+        return;
+    }
 
-
-
-
-
-
-
+    for (int i = start; i <= N; i++) {
+        arr.push_back(i);
+        dfs(i + 1);
+        arr.pop_back();
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    dfs(1);
+    return 0;
 }
